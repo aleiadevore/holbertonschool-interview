7256208653366be17899fb3c6954481969898822
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-20 11:48:42 -0500

    Not time efficient

---
+++ 0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Determines if all boxes can be opened, beginning with 0"""
+
+def canUnlockAll(boxes):
+    # array to store all keys captured
+    allKeys = {
+        0: True
+    }
+    #number of times going through loop
+    trips = 0
+
+    for i in range(1, len(boxes)):
+        allKeys[i] = False
+
+    #loop through all keys captured
+    while False in allKeys.values() and trips < len(boxes):
+        for i in allKeys:
+            if allKeys[i] == True:
+                for k in boxes[i]:
+                    allKeys[k] = True
+                trips += 1
+
+    if False in allKeys.values():
+        return False
+    return True    
