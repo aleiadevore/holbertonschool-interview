f88047b92a8a04626af9d2dc1141bfd044698b21
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-07-21 10:38:38 -0500

    Removed bug when keys given to boxes not there, pep8 fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """Determines if all boxes can be opened, beginning with 0"""
 
+
 def canUnlockAll(boxes):
     # array to store all keys captured
     allKeys = {
         0: True
     }
-    #number of times going through loop
+    # number of times going through loop
     trips = 0
 
     for i in range(1, len(boxes)):
         allKeys[i] = False
 
-    #loop through all keys captured
+    # loop through all keys captured
     while False in allKeys.values() and trips < len(boxes):
         for i in allKeys:
-            if allKeys[i] == True:
+            if allKeys[i] is True:
                 for k in boxes[i]:
-                    allKeys[k] = True
+                    if k > 0 and k < len(boxes):
+                        allKeys[k] = True
                 trips += 1
 
     if False in allKeys.values():
         return False
-    return True    
+    return True
