aa9f952742058fc0c0a4b1bd35499ff508c5a1fc
Aleia DeVore <2496@holbertonschool.com>
2021-08-05 22:40:17 +0000

    Always inserts on left

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -10,13 +10,90 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
+	heap_t *node = NULL, *new = NULL;
+
 	if (*root == NULL)
 	{
-		printf("No root\n");
 		*root = binary_tree_node(NULL, value);
-		printf("%d\n", (*root)->n);
+		if (*root == NULL)
+			return (NULL);
 		return (*root);
 	}
-	printf("Not null\n");
-	return (NULL);
+	// traverse tree to end
+	binary_tree_height(root);
+	node = *root;
+	while(1)
+	{
+		if (!node->left || !node->right)
+			break;
+		else
+			node = node->left;
+		
+	}
+
+	// add new node
+	new = binary_tree_node(node, value);
+	if (!new)
+		return (NULL);
+
+	// heapify
+
+	return (new);
 }
+
+/**
+ * swap - swaps values of two nodes
+ * 
+ * @a: first node
+ * @b: second node
+ * 
+ * Return: void
+ */
+
+void swap(binary_tree_t *a, binary_tree_t *b)
+{
+	int hold = a->n;
+
+	a->n = b->n;
+	b->n = hold;
+}
+
+/**
+ * height_finder - measure the height of binary tree
+ * @tree: pointer to root node of tree to measure height
+ * Return: if tree is NULL return 0
+ */
+
+size_t height_finder(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = height_finder(tree->left);
+	right = height_finder(tree->right);
+	if (left > right)
+		return (left + 1);
+	else
+		return (right + 1);
+}
+
+/**
+ * binary_tree_height - measure the height of binary tree
+ * @tree: pointer to root node of tree to measure height
+ * Return: if tree is NULL return 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t total;
+
+	total = height_finder(tree);
+
+	if (total == 0)
+		return (total);
+	return (total - 1);
+}
+// insert
+
+// heapify
