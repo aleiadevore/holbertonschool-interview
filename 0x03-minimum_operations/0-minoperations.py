a305c527e646e666c4dfc940a804b273fc58694b
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-12 15:47:43 -0500

    Using factorials and logs

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,8 +4,31 @@ import math
 
 
 def minOperations(n):
-    try:
-        ans = math.log(n, 2)
-        return round(ans * 2)
-    except:
+    if n <= 0 or n == 1:
         return 0
+    """if n % 2 == 0:
+        try:
+            ans = math.log(n, 2)
+            return round(ans * 2)
+        except:
+            return 0"""
+
+    if n == 3:
+        return 3
+    if n == 1:
+        return 0
+    cs = 2
+    ops = 0
+
+    while (cs * cs <= n):
+        if math.log(n, cs).is_integer():
+            ops += math.log(n, cs) * 2
+            return round(ops) + cs + 1
+        if (n % cs == 0):
+            n /= cs
+            ops += cs
+        else:
+            cs += 1
+    if n >= 2:
+        ops += n
+    return round(ops)
