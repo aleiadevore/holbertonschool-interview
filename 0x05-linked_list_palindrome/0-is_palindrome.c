0ca6a8f656dcf73ffe4d502b4a6e344c505234fb
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-24 14:37:05 -0500

    Betty fix

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -11,29 +11,28 @@
 
 int is_palindrome(listint_t **head)
 {
-    int i = 0, length;
-    int buffer[1024];
-    listint_t *node = *head;
+	int i = 0, length;
+	int buffer[1024];
+	listint_t *node = *head;
 
-    if (head == NULL)
-        return (1);
+	if (head == NULL)
+		return (1);
 
-    for (length = 0; length < 1024; length++)
-        buffer[length] = '\0';
+	for (length = 0; length < 1024; length++)
+		buffer[length] = '\0';
 
-    for (; node != NULL; node = node->next, i++)
-        buffer[i] = node->n;
+	for (; node != NULL; node = node->next, i++)
+		buffer[i] = node->n;
 
-    /* Set length, then traverse from both sides */
-    for (length = 0; buffer[length]; length++)
-        ;
+	/* Set length, then traverse from both sides */
+	for (length = 0; buffer[length]; length++)
+		;
 
-    for (i = 0, length--; length > 0; i++, length--)
-    {
-        if (buffer[i] != buffer[length])
-            return 0;
-    }
-    
+	for (i = 0, length--; length > 0; i++, length--)
+	{
+		if (buffer[i] != buffer[length])
+			return (0);
+	}
 
-    return 1;
+	return (1);
 }
