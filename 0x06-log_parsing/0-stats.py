d2844d54475dc29ec823dc58ef2cfb4a972e4dc2
aleiadevore <2496@holbertonschool.com>
2021-09-01 12:04:01 -0500

    Accounted for incorrect format

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -21,7 +21,9 @@ try:
     for line in fileinput.input():
         i += 1
         delim = line.split()
-        status[delim[7]] += 1
+        if len(delim) > 7:
+            if delim[7] in status.keys():
+                status[delim[7]] += 1
         if i % 10 == 0:
             filesize += int(delim[8])
             print("File size: {}".format(filesize))
