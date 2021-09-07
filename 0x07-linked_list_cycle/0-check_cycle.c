740dca3bdcb04f22531f834e23b148e875528e72
aleiadevore <2496@holbertonschool.com>
2021-09-07 09:23:16 -0500

    Checks if linked list has loop

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle: checks if linked list has a cycle in it
+ *
+ * @list: list to check
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list->next;
+
+	while (slow != NULL && fast != NULL)
+	{
+		if (slow->n == fast->n)
+			return (1);
+		slow = slow->next;
+		if (fast->next && fast->next->next)
+			fast = fast->next->next;
+		else
+			break;
+	}
+	return (0);
+}
