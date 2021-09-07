61c5ac7e04ffd9d8d0110fb42ec7d36882449e7c
aleiadevore <2496@holbertonschool.com>
2021-09-07 09:24:56 -0500

    Accounted for empty list

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -10,7 +10,12 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
-	listint_t *fast = list->next;
+	listint_t *fast;
+
+	if (list == NULL)
+		return 0;
+
+	fast = list->next;
 
 	while (slow != NULL && fast != NULL)
 	{
