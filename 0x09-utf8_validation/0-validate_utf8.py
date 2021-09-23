d780fdbac6bfafe24bedc1d7555a5ebb0e61c568
Aleia DeVore <2496@holbertonschool.com>
2021-09-23 14:20:19 +0000

    Needs more review

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,36 @@ Returns True if data is valid UTF-8 encoding, otherwise returns False
 """
 
 def validUTF8(data):
-    for item in data:
-        if (item >= 256):
-            return False
-    return True
+    # If byte starts UTF-8 char, process first N bits with N <= 4
+    # If N > 4, return False
+    N = 0
+
+    # Only check first two (most significant) bits
+    # Most significant must = 1
+    check1 = 1 << 7
+    # second most significant must = 0
+    check2 = 1 << 6
+
+    for i in data:
+        bit = 1 << 7
+        if N == 0:
+            while bit & i:
+                N += 1
+                bit = bit >> 1
+
+            # Char is only one byte
+            if N == 0:
+                continue
+
+            # Invalid byte
+            if N == 1 or N > 4:
+                return False
+        else:
+            # Checking if first two bits are correct
+            if not (i & check1 and not (i & check2)):
+                return False
+        # Repeat cycle for next largest bit in multibit char
+        N -= 1
+    return N == 0
+
+
