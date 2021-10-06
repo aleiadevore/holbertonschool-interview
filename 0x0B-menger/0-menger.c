1219c2aa699d6939e298fdd9fe22497374aed8ef
Aleia DeVore <2496@holbertonschool.com>
2021-10-06 01:59:12 +0000

    Works up to 1

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -10,7 +10,7 @@
 void menger(int level)
 {
 	char **sponge = NULL;
-	int row_size;
+	int row_size, i;
 
 	/* Special cases */
 	if (level < 0)
@@ -23,13 +23,22 @@ void menger(int level)
 
 	/* Malloc 3^level * 3^level matrix */
 	row_size = pow(3, level);
-	sponge = malloc(sizeof(char *) * row_size * row_size);
+	sponge = malloc(sizeof(char *) * row_size);
 	if (!sponge)
 		return;
-	printf("Row size: %d\n", row_size);
+	for (i = 0; i < row_size; i++)
+	{
+		sponge[i] = malloc(sizeof(char) * row_size);
+		if (!sponge[i])
+		{
+			free(sponge);
+			return;
+		}
+	}
 	make_menger(level, 1, sponge);
-	printf("row_size: %d\n", row_size);
 	print_menger(sponge, row_size);
+	for (i = 0; i < row_size; i++)
+		free(sponge[i]);
 	free(sponge);
 
 	/* WOULD IT BE BETTER TO FILL IN ALL WITH # AND THEN REPLACE WITH SPACE? */
@@ -42,7 +51,6 @@ void make_menger(int level, int place, char **sponge)
 
 	/* First level logic only */
 	subsize = pow(3, place);
-	printf("Subsize: %d\n", subsize);
 	sub_array = malloc(sizeof(char*) * subsize);
 	if (!sub_array)
 		return;
@@ -55,7 +63,6 @@ void make_menger(int level, int place, char **sponge)
 			return;
 		}
 	}
-	printf("Make malloc works\n");
 	for (i = 0; i < subsize; i++)
 	{
 		for (j = 0; j < subsize; j++)
@@ -69,13 +76,13 @@ void make_menger(int level, int place, char **sponge)
 
 	if (level == place)
 	{
-		sponge = sub_array;
 		for (i = 0; i < subsize; i++)
 		{
 			for (j = 0; j < subsize; j++)
-				printf("%c", sponge[i][j]);
-			printf("\n");
+				sponge[i][j] = sub_array[i][j];
 		}
+		for (i = 0; i < subsize; i++)
+			free(sub_array[i]);
 		free(sub_array);
 		return;
 	}
@@ -99,18 +106,10 @@ void print_menger(char **sponge, int row_size)
 {
 	int i, j;
 
-	printf("In print\nTesttest\n");
-	fflush(stdout);
-	printf("Test");
-	printf("row_size: %d", row_size);
 	for (i = 0; i < row_size; i++)
 	{
-		printf("i: %d", i);
 		for (j = 0; j < row_size; j++)
-		{
-			printf("j: %d", j);
 			putchar(sponge[i][j]);
-		}
 		printf("\n");
 	}
 }
