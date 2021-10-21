d7f974d5654fe51c01372c734a114bb329822688
aleiadevore <2496@holbertonschool.com>
2021-10-21 11:54:20 -0500

    Works for 4 but gets nothing for 6?

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -15,3 +15,47 @@ if __name__ == "__main__":
     if (N < 4):
         print("N must be at least 4")
         exit(1)
+
+    """First approach -- brute force and backtracking"""
+    allQueens = dict()
+
+    def ResetDict():
+        # Create dict to store locations of queens
+        for i in range(0, N):
+            allQueens[i] = ()
+
+    def IterateFirst(startJ):
+        usedJ = []
+        allQueens[0] = (0, startJ)
+        usedJ.append(startJ)
+
+        for i in range(1, N):
+            if len(allQueens[i - 1]) != 2:
+                # print("Resetting")
+                return False
+            for j in range(0, N):
+                # print("[{}, {}]".format(i, j))
+                if j in usedJ:
+                    # print("j has been used")
+                    continue
+                if i > 0:
+                    prevJ = allQueens[i - 1][1]
+                    if j == prevJ - 1 or j == prevJ + 1:
+                        # print("j is {} and prevJ is {}".format(j, prevJ))
+                        continue
+                allQueens[i] = (i, j)
+                usedJ.append(j)
+                if (i == N - 1):
+                    return True
+                i += 1
+
+    def PrintDict():
+        print("[", end="")
+        print(", ".join("[{}, {}]".format(
+            v[0], v[1]) for k, v in allQueens.items()), end="]\n")
+
+    for startJ in range(0, N):
+        # print("In outer loop, startJ is {}".format(startJ))
+        ResetDict()
+        if IterateFirst(startJ):
+            PrintDict()
