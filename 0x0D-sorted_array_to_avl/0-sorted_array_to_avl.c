8906edc0bf6064fe16ce5da6af9d7819ad9c3bb5
aleiadevore <2496@holbertonschool.com>
2021-10-25 16:38:19 -0500

    Works as long as you're okay losing all your memory

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -16,26 +16,46 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	avl_t *root = NULL;
 
-	root = recursive_call(array, array[0], array[size - 1]);
+	root = recursive_call(array, 0, size - 1);
 	return (root);
 }
+
+/**
+ * recursive_call - recursive engine of algorithm
+ * @array: array to turn into tree
+ * @start: starting index
+ * @end: ending index
+ *
+ * Return: root of tree
+ */
 
 avl_t *recursive_call(int *array, int start, int end)
 {
 	int mid;
-	avl_t *root;
+	avl_t *root = NULL;
 
+	/*printf("Start: %d, End: %d\n", start, end);*/
 	if (start > end)
 		return (NULL);
 
-	mid = (start + end)/2;
+	mid = (start + end) / 2;
+	/*printf("Mid: %d\n", mid);*/
 	root = newNode(array[mid]);
+	if (!root)
+		return (NULL);
 
 	root->left = recursive_call(array, start, mid - 1);
 	root->right = recursive_call(array, mid + 1, end);
 
-	return root;
+	return (root);
 }
+
+/**
+ * newNode - creates a new node
+ * @n: n value of node
+ *
+ * Return: pointer to node
+ */
 
 avl_t *newNode(int n)
 {
@@ -47,6 +67,8 @@ avl_t *newNode(int n)
 	node->n = n;
 	node->left = NULL;
 	node->right = NULL;
+
+	/*printf("Created node %d\n", node->n);*/
 
 	return (node);
 }
