7080fed010aef263ae3421321a9fab86306d48db
aleiadevore <2496@holbertonschool.com>
2021-10-25 15:42:31 -0500

    Fixed tabs for betty

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -2,13 +2,18 @@
 
 /**
  * sorted_array_to_avl - builds AVL tree from array
- * @array: array to turn into tree
+ * @array: pointer to first element in array to convert
  * @size: size of array
  *
  * Return: avl tree
  */
 
+/* assume no duplicate value in array */
+/* not allowed to rotate */
+/* Can only have three functions in file */
+
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
+
 
 }
