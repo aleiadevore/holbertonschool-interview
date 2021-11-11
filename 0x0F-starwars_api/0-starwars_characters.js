bb97d0ce8edff9f7f7738c38ac3acd589fe36703
Aleia DeVore <2496@holbertonschool.com>
2021-11-11 17:46:22 +0000

    Sorted

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -8,14 +8,27 @@ const episode = process.argv[2];
 // Display one character name per line
 // Characters in same order as /films/ endpoint
 const url = 'https://swapi-api.hbtn.io/api/films/'.concat(episode);
-request(url, function(error, response, body) {
-    if (error) throw error;
-    const people = JSON.parse(body).characters;
+request(url, function (error, response, body) {
+  if (error) throw error;
+  const people = JSON.parse(body).characters;
+  const toSort = {};
 
-    people.forEach(character => {
-        request(character, function(error, response, body) {
-            if (error) throw error;
-            console.log(JSON.parse(body).name);
-        });
+  // Call each character endpoint, connect name with output
+  people.forEach(character => {
+    request(character, function (error, response, body) {
+      if (error) throw error;
+      toSort[character] = JSON.parse(body).name;
+
+      if (Object.keys(toSort).length === people.length) {
+        printSorted(people, toSort);
+      }
     });
+  });
+
+  function printSorted (people, toSort) {
+    // Sort by character number
+    people.forEach(character => {
+      console.log(toSort[character]);
+    });
+  }
 });
