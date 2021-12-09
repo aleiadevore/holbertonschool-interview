41ac41dde624a4590ff3437bb91b0b2c79367586
Aleia DeVore <2496@holbertonschool.com>
2021-12-09 19:28:06 +0000

    Finally works

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,26 +5,26 @@ import requests
 
 
 def count_words(subreddit, word_list, after=None, answer_dict={}):
-    """ Prints number of keywords given for a subreddit's hot list """
+    """ Prints number of keywords given for a subreddits hot list """
 
     # subreddit: subreddit to check
     # word_list: set of keywords to check for
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(
-                subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "user"}
     parameters = {"show": "all", "next": "next", "after": after}
 
     try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False,
-                                params=parameters).json()
-    except ValueError:
-        return
+            response = requests.get(url, headers=headers,
+                                    allow_redirects=False,
+                                    params=parameters).json()
+    except:
+            return
 
     for item in word_list:
         # adding all keywords to answer_dict with a count of 0
         if item.lower() not in answer_dict:
+            # adding all keywords to answer_dict with a count of 0
             answer_dict[item.lower()] = 0
 
     # Accounting for errors
@@ -41,17 +41,17 @@ def count_words(subreddit, word_list, after=None, answer_dict={}):
                 if key == item.lower():
                     answer_dict[key] += 1
 
-        # Saving next page
-        after = response.get("data").get("after")
+    # Saving next page
+    after = response.get("data").get("after")
 
-        if not after:
-            # no more pages to check, print answers
-            for key, value in sorted(
-                                answer_dict.items(),
-                                key=lambda item: item[1], reverse=True):
+    if not after:
+        # no more pages to check, print answers
+        for key, value in sorted(
+            answer_dict.items(),
+                key=lambda item: item[1], reverse=True):
                 if value != 0:
                     print("{}: {}".format(key, value))
-            return
+        return
 
-        # If there are more pages to check, make recursive call
-        return count_words(subreddit, word_list, after, answer_dict)
+    # If there are more pages to check, make recursive call
+    return count_words(subreddit, word_list, after, answer_dict)
