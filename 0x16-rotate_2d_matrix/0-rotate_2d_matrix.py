64aa0a7810423f38350d0ada3852c6d644dbf6bf
Aleia DeVore <2496@holbertonschool.com>
2022-01-03 21:46:26 +0000

    Works with current tests

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,8 +3,18 @@
 Rotates a matrix 90 degrees clockwise
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates (in place) a matrix 90 degrees clockwise
     """
-    pass
+    # Rows will become columns, with first row being last column
+    # Easier to manip: columns will become rows, w/ first col as first row
+
+    # reversed: makes shallow reversed copy
+    # *: unpacks each sublist
+    # zip: transposes by making tuples with one item from each list
+    new = list(zip(*reversed(matrix)))
+    for i in range(0, len(new) - 1):
+        for j in range(0, len(new[0]) - 1):
+            matrix[i][j] = new[i][j]
