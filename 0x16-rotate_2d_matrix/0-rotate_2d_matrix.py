38249429d8044c90647f0bc5da738072f8892533
Aleia DeVore <2496@holbertonschool.com>
2022-01-03 19:00:51 +0000

    Prepped for algorithm practice

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Rotates a matrix 90 degrees clockwise
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates (in place) a matrix 90 degrees clockwise
+    """
+    pass
---
+++ 0x16-rotate_2d_matrix/README.md
@@ -0,0 +1,3 @@
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+
+
