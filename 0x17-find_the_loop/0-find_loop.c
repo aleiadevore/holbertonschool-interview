cd8a20d90313bf1b0a9ba7837391f92d434d9e8d
Aleia DeVore <2496@holbertonschool.com>
2022-01-11 16:57:18 +0000

    Static library had compile error. Switching to source code.

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds loop in linked list
+ * @head: head of linked list
+ *
+ * Return: address of node where loop starts or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+    if (!head)
+        return (NULL);
+    return (NULL);
+}
