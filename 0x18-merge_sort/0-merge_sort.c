4a14b2ed2903c132d4192120828021a1d491259d
Aleia DeVore <2496@holbertonschool.com>
2022-01-17 17:45:13 +0000

    Adding notes

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+/* Size of left should be larger than size of right */
+/* Sort left before sorting right */
+
+/*
+ * Algorithm: Recursively divide halves until size of array == 1
+ * then start merging back together until array complete
+*/
+
+/**
+ * merge_sort - sorts array of int using merge sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+
+void merge_sort(int *array, size_t size)
+{
+
+}
