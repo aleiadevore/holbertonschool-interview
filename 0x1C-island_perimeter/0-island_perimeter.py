577acefb848551c69b0d9faee376a473128ce4a2
Aleia DeVore <2496@holbertonschool.com>
2022-02-16 16:55:00 +0000

    Fixing line length

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -12,21 +12,27 @@ def island_perimeter(grid):
     if grid == None:
         return 0
     p = 0
-    for line in range(0, len(grid) - 1):
-        for num in range(0, len(grid[line]) - 1):
+    for line in range(0, len(grid)):
+        for num in range(0, len(grid[line])):
+            # print("Line [{0}][{1}]".format(line, num))
             if grid[line][num] == 1:
                 p += 4
+                # print("Adding 4. p = {0}".format(p))
                 # Check for intersections with other 1s
                 if line > 0:
                     if grid[line - 1][num] == 1:
                         p -= 1
+                        # print("Land above. p = {0}".format(p))
                 if line != len(grid) - 1:
                     if grid[line + 1][num] == 1:
                         p -= 1
+                        # print("Land below. p = {0}".format(p))
                 if num > 0:
                     if grid[line][num - 1]:
                         p -= 1
+                        # print("Land left. p = {0}".format(p))
                 if num != len(grid[line]) - 1:
                    if grid[line][num + 1]:
-                        p -= 1 
+                        p -= 1
+                        # print("Land right. p = {0}".format(p))
     return p
