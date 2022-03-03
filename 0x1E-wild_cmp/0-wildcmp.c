ddb6d48c8f2fabf9fddce5f441fd3ed587ac911e
aleiadevore <2496@holbertonschool.com>
2022-03-03 17:54:33 -0600

    Switched to recursive, need to handle wildcard

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -10,44 +10,15 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	/* First working with no wildcard */
-
-	for (i = 0, j = 0; s1[i] != 0; i++, j++)
-	{
-		/* If s2 runs out of chars before checking complete */
-		if (!s2[j])
-		{
-			/*printf("s2 out of chars\n");*/
-			return (0);
-		}
-		if (s1[i] != s2[j])
-		{
-			/* If difference isn't caused by wildcard */
-			if (s2[j] != 42)
-			{
-				/*printf("%s: i = %d\n", s1, i);
-				printf("%c doesn't = %c\n", s1[i], s2[j]);*/
-				return (0);
-			}
-			/* If diff is *, iterate through s1 until strings match again */
-			/*printf("\nIterating s2\n");*/
-			while (s2[j] == 42)
-			{
-				/*printf("%c\n", s2[j]);*/
-				if (s2[j] == 0)
-					return (1);
-				j++;
-			}
-			/*printf("\nIterating s1\n");*/
-			while (s1[i] != s2[j] && s1[i] != 0)
-			{
-				/*printf("%c != %c\n", s1[i], s2[j]);*/
-				i++;
-			}
-		}
-	}
-
+	/* If it's reached this point, it's a match */
+	if (*s1 == 0 && *s2 == 0)
+		return (1);
+	/* If characters are the same, check with next chars */
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 2));
+	
+	/* Check for wildcard */
+	if (*s2 != '*')
+		return (0);
 	return (1);
 }
